584e255c1130e0d5f93c047b2250c04d329d3132
tookiewill33 <157699175+tookiewill33@users.noreply.github.com>
2024-02-18 15:59:17 -0500

    Update saturdayApp.groovy

--- vars/saturdayApp.groovy
+++ vars/saturdayApp.groovy
@@ -1,29 +1,24 @@
 def call(String repoUrl){
-  pipeline{
-	agent any 
-	stages{
-		stage('1-git-clone'){
-			steps{
-				sh 'git --version'
-				sh 'git clone'
-			}
-		}
-		stage('2-mavenbuild'){
-			steps{
-				sh 'mvn package'
-			}
-		}
-		stage('3-codeQuality'){
-			steps{
-				sh 'mvn sonar:sonar'
-			}
-		}
-		stage('4-deploy'){
-			steps{
-				sh 'mvn deploy'
-			}
-		}
-	}	
-	
+  pipeline {
+       agent any
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
 }
  }
